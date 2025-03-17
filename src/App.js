664a58e670eff30b158a672c692e8ecca44927da
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-03-17 10:19:04 +0530

    Super admin login code prettier

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
             <Alert />
           </AlertProvider>
         </AppProvider>
-      </div>{" "}
+      </div>
     </>
   );
 }
--- src/Components/Login/LoginPage.js
+++ src/Components/Login/LoginPage.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import loginHeroImage from "../../Assets/Login/loginHeroImage.png"
-import LoginForm from './LoginForm'
+import React from "react";
+import loginHeroImage from "../../Assets/Login/loginHeroImage.png";
+import LoginForm from "./LoginForm";
 
 const Hero = () => {
-
-    return (
-        <div className='w-full hidden md:block md:w-fit h-[50vh] md:min-w-[calc(100vh*3/4)] md:aspect-[3/4] md:h-full overflow-hidden bg-accent-indigo'>
-            <img
-                src={loginHeroImage}
-                className='w-full h-full'
-                alt=''
-            />
-        </div>
-    )
-}
+  return (
+    <div className="w-full hidden md:block md:w-fit h-[50vh] md:min-w-[calc(100vh*3/4)] md:aspect-[3/4] md:h-full overflow-hidden bg-accent-indigo">
+      <img src={loginHeroImage} className="w-full h-full" alt="" />
+    </div>
+  );
+};
 
 const LoginPage = () => {
-
   return (
-    <div className='w-full min-h-screen md:h-screen overflow-auto flex flex-col md:flex-row items-center md:justify-start bg-white'>
-        <Hero />
-        <LoginForm />
+    <div className="w-full min-h-screen md:h-screen overflow-auto flex flex-col md:flex-row items-center md:justify-start bg-white">
+      <Hero />
+      <LoginForm />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
